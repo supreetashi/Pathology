18dfae5416c6d2b7844676b87c034d29cbf6f844
shradhamathpati <shradhamathpati03@gmail.com>
2026-06-09 21:40:39 +0530

    Integerated receive page

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,25 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
-import clinicReducer           from "./clinicSlice";
-import sampleTubeReducer       from "./sampleTubeSlice";
+import clinicReducer from "./clinicSlice";
+import sampleTubeReducer from "./sampleTubeSlice";
 import pathologyProfileReducer from "./pathologyProfileSlice";
-import testReducer             from "./testSlice";
-import parameterReducer        from "./parameterSlice";
-import machineReducer          from "./MachineSlice";
-import ordersReducer           from "./orders.slice";
-import shipmentReducer         from "./shipment.slice";
-import templateReducer from "./templateSlice"
+import testReducer from "./testSlice";
+import parameterReducer from "./parameterSlice";
+import machineReducer from "./MachineSlice";
+import ordersReducer from "./orders.slice";
+import shipmentReducer from "./shipment.slice";
+import templateReducer from "./templateSlice";
+import receiveReducer from "./ReceiveSlice";
 
 export const store = configureStore({
   reducer: {
-    clinic:           clinicReducer,
-    sampleTube:       sampleTubeReducer,
+    clinic: clinicReducer,
+    sampleTube: sampleTubeReducer,
     pathologyProfile: pathologyProfileReducer,
-    test:             testReducer,
-    parameter:        parameterReducer,
-    machine:          machineReducer,
-    orders:           ordersReducer,
-    shipment:         shipmentReducer,
+    test: testReducer,
+    parameter: parameterReducer,
+    machine: machineReducer,
+    orders: ordersReducer,
+    shipment: shipmentReducer,
     template: templateReducer,
+    receive: receiveReducer,
   },
 });
 
